5ee91648fff34bf4b46c8d8d6d911d816e297234
NurbolatRustamov <nurbolatrustamov1@gmail.com>
2024-06-04 12:13:43 +0500

    2 masala

---
+++ index.js
@@ -0,0 +1,35 @@
+
+
+// function move(position, roll, link, FAQ, ass) {
+//     return (position + roll + link + FAQ + ass) * roll;
+// }
+// let position = prompt("sonni kiriting")
+// let roll = prompt("sonni kiriting")
+// let link = prompt("sonni kiriting")
+// let FAQ = prompt("sonni kiriting")
+// let ass = prompt("sonni kiriting")
+
+
+// position = Number(position)
+// roll = Number(roll)
+// link = Number(link)
+// FAQ = Number(FAQ)
+// ass = Number(ass)
+
+// console.log(move(position, roll, link, FAQ, ass));
+
+
+
+function move(position, roll,) {
+    return (position * 2 + roll);
+}
+let position = prompt("sonni kiriting")
+let roll = prompt("sonni kiriting")
+
+
+
+position = Number(position)
+roll = Number(roll)
+
+
+console.log(move(position, roll,));
